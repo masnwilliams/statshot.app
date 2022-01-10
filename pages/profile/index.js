8413f64143ac167661ea01a462456cddf701ab59
Mason Williams <mwilliams@vantagetechnologies.io>
2022-01-10 10:05:29 -0500

    Improve load times using StaticPath/StaticProp

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,75 +1,15 @@
-import MatchFeed from "@components/MatchFeed";
-import PublicUserProfile from "@components/PublicUserProfile"
 import Metatags from "@components/Metatags";
-import { getUserWithUsername } from "@lib/firebase";
-import { getUserCSRS, getUserMatchList, getUserMultiplayerServiceRecord } from '@lib/helper';
-  
-export async function getServerSideProps() {
+import { useContext } from "react";
+import { UserContext } from "@lib/context";
 
-  const username = 'liquidforce134';
-  const userDoc = await getUserWithUsername(username);
+export default function UserProfilePage() {
 
-  // If no user, short circuit to 404 page
-  if (!userDoc) {
-    return {
-      notFound: true,
-    };
-  }
-
-  // JSON serializable data
-  let user = null;
-
-  if (userDoc) {
-    user = userDoc.data();
-  }
-
-  const matchData = {
-    gamertag: username,
-    match_count: 25,
-    match_offset: 0,
-    mode: "matchmade"
-  };
-
-  const userSRData = {
-    gamertag: username,
-    filter: "matchmade:ranked"
-  };
-
-  const userCsrData = {
-    gamertag: username,
-    filter: "matchmade:ranked"
-  };
-
-  const matches = await getUserMatchList(matchData);
-  const playerSR = await getUserMultiplayerServiceRecord(userSRData);
-  const playerCSR = await getUserCSRS(userCsrData);
-
-  return {
-    props: { 
-      user, 
-      matches, 
-      playerSR, 
-      playerCSR 
-    }, // will be passed to the page component as props
-  };
-}
-
-export default function UserProfilePage({ 
-  user, 
-  matches, 
-  playerSR, 
-  playerCSR 
-}) {
+  const { user, username } = useContext(UserContext);
 
   return (
     <main>
-      <Metatags title={user.username} description={`${user.username}'s profile`} />
-      Hello {user.displayName}
-      <PublicUserProfile playerSR={playerSR} playerCSR={playerCSR} />
-
-      <h2>Recent Matches</h2>
-
-      <MatchFeed matches={matches} />
+      <Metatags title={username} description={`${username}'s profile`} />
+      Hello {username}
     </main>
   );
 }
